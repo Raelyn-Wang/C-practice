70bc51fecc228822696583d8e7709258918f8277
Raelyn-Wang <r80171@gmail.com>
2026-03-30 01:27:39 +0800

    Update 12.1.main.c

--- practice/12.1.main.c
+++ practice/12.1.main.c
@@ -8,11 +8,12 @@ int main(){
     scanf("%d",&a);
     scanf("%d",&b);
     
-    while(b!=0){
+    while(1){
         r=a%b;
+        if(r==0) break;
         a=b;
         b=r;
     }
     
-    printf("GCB=%d",a);
+    printf("GCB=%d",b);
 }
